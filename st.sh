9ee196180034e4f38c49f348b700c87dc9d2dee4
user <user@email>
2020-05-06 01:15:11 +0300

    add client.py file

--- st.sh
+++ st.sh
@@ -1,6 +1,8 @@
 gunicorn --build 127.0.0.1:5000 wsgi:app & APP_PID=$!
 sleep 5
+echo start client
+python3 client.py
+sleep 5
 echo $APP_PID
 kill -TERM $APP_PID
-echo process gunicorns kills
 exit 0
